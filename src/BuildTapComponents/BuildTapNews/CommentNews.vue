a82d1795786d61d28835197b7dda3ce9d2c964d2
Trung <nguyentrung250302@gmail.com>
2023-05-10 19:33:56 +0700

    additional

---
+++ src/BuildTapComponents/BuildTapNews/CommentNews.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="comment-container">
+    <h2 class="comment-heading">Bình luận</h2>
+    <div class="comment-list">
+      <div v-for="comment in comments" :key="comment.id" class="comment-item">
+        <strong class="comment-author">Name: {{ comment.author }}</strong>
+        <p class="comment-content">Comment: {{ comment.content }}</p>
+      </div>
+    </div>
+    <form class="comment-form" @submit.prevent="addComment">
+      <input v-model="newComment.author" placeholder="Tên người dùng" required />
+      <textarea v-model="newComment.content" placeholder="Nội dung bình luận" required></textarea>
+      <button type="submit" class="comment-submit">Gửi</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comments: [
+        { id: 1, author: "những chú chim lạc lối", content: "hay" },
+        { id: 2, author: "Hanna", content: "Ok" }
+      ],
+      newComment: { author: "", content: "" }
+    };
+  },
+  methods: {
+    addComment() {
+      if (this.newComment.author && this.newComment.content) {
+        const newId = this.comments.length + 1;
+        const newComment = { id: newId, ...this.newComment };
+        this.comments.push(newComment);
+        this.newComment.author = "";
+        this.newComment.content = "";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.comment-container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.comment-heading {
+  text-align: center;
+  color: #333;
+}
+
+.comment-list {
+  margin-bottom: 20px;
+}
+
+.comment-item {
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #f5f5f5;
+  border-radius: 5px;
+}
+
+.comment-author {
+  font-weight: bold;
+  color: #555;
+}
+
+.comment-content {
+  margin-top: 5px;
+  color: #333;
+}
+
+.comment-form input,
+.comment-form textarea {
+  display: block;
+  margin-bottom: 10px;
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.comment-form textarea {
+  resize: vertical;
+}
+
+.comment-submit {
+  padding: 8px 15px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 200px;
+}
+
+.comment-submit:hover {
+  background-color: #0056b3;
+}
+h2 {
+    font-weight: 700;
+    font-size:  30px;
+    color: aquamarine;
+}
+</style>
--- src/BuildTapComponents/BuildTapNews/NewsLetter1.vue
+++ src/BuildTapComponents/BuildTapNews/NewsLetter1.vue
@@ -5,15 +5,55 @@
     <div class="newsletter0">
       <h1>{{ newsList.length > 0 ? newsList[0].title : "Loading..." }}</h1>
       <img
+        id="img-center"
         :src="newsList.length > 0 ? newsList[0].thumbnail : ''"
         alt="img 0"
       />
       <h2>{{ newsList.length > 0 ? newsList[0].caption : "Loading..." }}</h2>
-      <p> Trang web liên quan:
-      <a href="https://thaycaoanh.vn/tu-vi-1/xem-tu-vi-tron-doi-cua-12-con-giap-chinh-xac-nhat-2/">
-      {{newsList.length > 0 ? newsList[0].url : "Loading..."}}
-      </a>
+      <p>
+        Trang web liên quan:
+        <a
+          href="https://thaycaoanh.vn/tu-vi-1/xem-tu-vi-tron-doi-cua-12-con-giap-chinh-xac-nhat-2/"
+        >
+          {{ newsList.length > 0 ? newsList[0].url : "Loading..." }}
+        </a>
       </p>
+    </div>
+    <!-- comment -->
+    <comment-news />
+    <!-- related-info -->
+    <div class="related-information">
+      <h3>Tin liên quan</h3>
+      <div class="related-info">
+        <img
+          class="img"
+          :src="newsList.length > 0 ? newsList[1].thumbnail : ''"
+          alt="img 0"
+        />
+        <a href="" @:click="changeTapNews1">{{
+          newsList.length > 0 ? newsList[1].title : "Loading..."
+        }}</a>
+      </div>
+      <div class="related-info">
+        <img
+          class="img"
+          :src="newsList.length > 0 ? newsList[2].thumbnail : ''"
+          alt="img 0"
+        />
+        <a href="" @:click="changeTapNews1">{{
+          newsList.length > 0 ? newsList[2].title : "Loading..."
+        }}</a>
+      </div>
+      <div class="related-info">
+        <img
+          class="img"
+          :src="newsList.length > 0 ? newsList[4].thumbnail : ''"
+          alt="img 0"
+        />
+        <a href="" @:click="changeTapNews1">{{
+          newsList.length > 0 ? newsList[4].title : "Loading..."
+        }}</a>
+      </div>
     </div>
     <!--  -->
     <footer-view />
@@ -23,11 +63,13 @@
 <script>
 import axios from "axios";
 import NavBarHeader from "@/components/MyArchive/headerSolution/NavBarHeader.vue";
-import FooterView from '@/components/MyArchive/FooterView.vue';
+import FooterView from "@/components/MyArchive/FooterView.vue";
+import CommentNews from "@/BuildTapComponents/BuildTapNews/CommentNews.vue";
 export default {
   components: {
     NavBarHeader,
     FooterView,
+    CommentNews,
   },
   data() {
     return {
@@ -58,6 +100,17 @@ export default {
         console.error(error);
       });
   },
+  methods: {
+    changeTapNews1() {
+      this.$router.push("/news2");
+    },
+    changeTapNews2() {
+      this.$router.push("/news3");
+    },
+    changeTapNews3() {
+      this.$router.push("/news4");
+    },
+  },
 };
 </script>
 
@@ -68,6 +121,16 @@ export default {
 }
 .newsletter0 {
   margin: 0 auto;
+}
+.related-information {
+  width: 700px;
+  height: 500px;
+  margin: 30px auto;
+  border: solid 1px silver;
+}
+.related-info {
+  margin: 20px 20px;
+  display: flex;
 }
 h1 {
   display: flex;
@@ -82,14 +145,31 @@ h2 {
   font-weight: 700;
   font-size: 20px;
   margin: 40px auto;
-  color:  rgb(51,51,64);
+  color: rgb(51, 51, 64);
 }
-img {
+h3 {
+  border-bottom: solid 1px silver;
+  margin: 0 20px;
+  padding: 20px 0;
+  font-weight: 500;
+  font-size: 20px;
+}
+.img {
+  width: 200px;
+  height: 100px;
+}
+#img-center {
   margin: 0 auto;
 }
 p {
   font-size: 18px;
   font-weight: 500;
+  margin: 20px auto;
+}
+a {
+  margin: 0 15px;
+  font-weight: 500;
+  font-size: 20px;
 }
 a:hover {
   color: rgb(0, 47, 255);
